61813903f4181c6cd21554a1db8136f8a81a72b1
João Ricardo Lourenço <jorl17.8@gmail.com>
2017-09-16 04:17:31 +0100

    Fix 400 errors instead generating 500 due to Bottle trying to parse data as HTML

--- server.py
+++ server.py
@@ -1,21 +1,27 @@
+import json
+
 import bottle
-from bottle import route, run, request
+from bottle import route, run, request, response
 
 from gdal_interfaces import GDALTileInterface
 
 interface = GDALTileInterface('data/', 'data/summary.json')
 interface.create_summary_json()
 
+class InternalException(ValueError):
+    pass
+
+
 def get_elevation(location_with_comma):
     try:
         lat, lng = [float(i) for i in location_with_comma.split(',')]
     except:
-        return bottle.HTTPResponse(status=400, body={'error': 'Bad parameter format "%s".' % location_with_comma})
+        raise InternalException(json.dumps({'error': 'Bad parameter format "%s".' % location_with_comma}))
 
     try:
         elevation = interface.lookup(lat, lng)
     except:
-        return bottle.HTTPResponse(status=404, body={'error': 'No such coordinate (%s, %s)' % (lat, lng)})
+        raise InternalException(json.dumps({'error': 'No such coordinate (%s, %s)' % (lat, lng)}))
 
     return {
         'latitude': lat,
@@ -28,9 +34,16 @@ def lookup():
     locations = request.query.locations
 
     if not locations:
-        return bottle.HTTPResponse(status=400, body={'error': '"Locations" is required.'})
+        response.status = 400
+        response.content_type = 'application/json'
+        return json.dumps({'error': '"Locations" is required.'})
 
-    return {'results': [get_elevation(l) for l in locations.split('|')]}
+    try:
+        return {'results': [get_elevation(l) for l in locations.split('|')]}
+    except InternalException as e:
+        response.status = 400
+        response.content_type = 'application/json'
+        return e.args[0]
 
 #run(host='0.0.0.0', port=8080)
 run(host='0.0.0.0', port=8080, server='gunicorn', workers=4)
