e1e633d51ebf73bf84294bdc5e2da23491edb14f
João Ricardo Lourenço <jorl17.8@gmail.com>
2018-02-07 02:46:11 +0000

    Fail on first error in shell scripts. Use unar for rar3.0+ compatibility in docker image

--- download-srtm-data.sh
+++ download-srtm-data.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
+set -eu
+
 wget http://gisweb.ciat.cgiar.org/TRMM/SRTM_Resampled_250m/SRTM_NE_250m_TIF.rar && \
 wget http://gisweb.ciat.cgiar.org/TRMM/SRTM_Resampled_250m/SRTM_SE_250m_TIF.rar && \
 wget http://gisweb.ciat.cgiar.org/TRMM/SRTM_Resampled_250m/SRTM_W_250m_TIF.rar && \
-unrar e -y SRTM_NE_250m_TIF.rar && \
-unrar e -y SRTM_SE_250m_TIF.rar && \
-unrar e -y SRTM_W_250m_TIF.rar
+unar -f SRTM_NE_250m_TIF.rar && \
+unar -f SRTM_SE_250m_TIF.rar && \
+unar -f SRTM_W_250m_TIF.rar
