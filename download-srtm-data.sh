3ebe46843d9c8b4108276a3b837cad76a898f491
zstadler <zeev.stadler@gmail.com>
2021-07-10 09:26:19 +0300

    Update source of worldwide SRTM 250m dataset
    Resolve #42

--- download-srtm-data.sh
+++ download-srtm-data.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 
 set -eu
-
-wget http://gisweb.ciat.cgiar.org/TRMM/SRTM_Resampled_250m/SRTM_NE_250m_TIF.rar && \
-wget http://gisweb.ciat.cgiar.org/TRMM/SRTM_Resampled_250m/SRTM_SE_250m_TIF.rar && \
-wget http://gisweb.ciat.cgiar.org/TRMM/SRTM_Resampled_250m/SRTM_W_250m_TIF.rar && \
+wget https://srtm.csi.cgiar.org/wp-content/uploads/files/250m/SRTM_NE_250m_TIF.rar && \
+wget https://srtm.csi.cgiar.org/wp-content/uploads/files/250m/SRTM_SE_250m_TIF.rar && \
+wget https://srtm.csi.cgiar.org/wp-content/uploads/files/250m/SRTM_W_250m_TIF.rar && \
 unar -f SRTM_NE_250m_TIF.rar && \
 unar -f SRTM_SE_250m_TIF.rar && \
 unar -f SRTM_W_250m_TIF.rar
