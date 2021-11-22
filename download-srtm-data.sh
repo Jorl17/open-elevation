37976917d64eada8ddbe1dfeb8935911d78e21e0
Lucas Fischer <lucasfischerpt@gmail.com>
2021-11-22 16:41:12 +0000

    Use -D option in unar as suggested by charlycou

--- download-srtm-data.sh
+++ download-srtm-data.sh
@@ -4,6 +4,6 @@ set -eu
 wget https://srtm.csi.cgiar.org/wp-content/uploads/files/250m/SRTM_NE_250m_TIF.rar && \
 wget https://srtm.csi.cgiar.org/wp-content/uploads/files/250m/SRTM_SE_250m_TIF.rar && \
 wget https://srtm.csi.cgiar.org/wp-content/uploads/files/250m/SRTM_W_250m_TIF.rar && \
-unar -f SRTM_NE_250m_TIF.rar && \
-unar -f SRTM_SE_250m_TIF.rar && \
-unar -f SRTM_W_250m_TIF.rar
+unar -D -f SRTM_NE_250m_TIF.rar && \
+unar -D -f SRTM_SE_250m_TIF.rar && \
+unar -D -f SRTM_W_250m_TIF.rar
