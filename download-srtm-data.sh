f4225eb62a0793556eac5586751fc52c3a563a35
Edgar Sanou <edgar.sanou@mecalux.com>
2023-04-24 13:39:33 +0200

    Dockerfile fixes, download script update and req update
    Dockerfile updated fixing gdal dependences and avoiding TZdata install problems with timezone
    Updated pip requeriments to fix "no module named _gdal_array"
    And download-srtm-data script now moves all tif archives to data folder becose unar makes one folder for every rar

--- download-srtm-data.sh
+++ download-srtm-data.sh
@@ -7,3 +7,6 @@ wget https://srtm.csi.cgiar.org/wp-content/uploads/files/250m/SRTM_W_250m_TIF.ra
 unar -f SRTM_NE_250m_TIF.rar && \
 unar -f SRTM_SE_250m_TIF.rar && \
 unar -f SRTM_W_250m_TIF.rar
+mv SRTM_NE_250m_TIF/SRTM_NE_250m.tif .
+mv SRTM_SE_250m_TIF/SRTM_SE_250m.tif .
+mv SRTM_W_250m_TIF/SRTM_W_250m.tif .
