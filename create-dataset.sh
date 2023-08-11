b1e587b65873f6c4c82c32f751cfc7be04caf3ea
lsfischer <lucasfischerpt@gmail.com>
2023-08-11 20:12:14 +0100

    Also remove README.md

--- create-dataset.sh
+++ create-dataset.sh
@@ -12,6 +12,6 @@ cd $OUTDIR
 ../create-tiles.sh ./SRTM_NE_250m_TIF/SRTM_NE_250m.tif 10 10
 ../create-tiles.sh ./SRTM_SE_250m_TIF/SRTM_SE_250m.tif 10 10
 ../create-tiles.sh ./SRTM_W_250m_TIF/SRTM_W_250m.tif 10 20
-rm -rf SRTM_NE_250m.tif SRTM_SE_250m.tif SRTM_W_250m.tif *.rar
+rm -rf SRTM_NE_250m.tif SRTM_SE_250m.tif SRTM_W_250m.tif *.rar *.md
 
 cd $CUR_DIR
